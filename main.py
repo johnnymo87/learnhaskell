afa32ea3d94c4ab5a6c8e3ba5f8fd5a7f957ece9
johnnymo87 <johnnymo87@gmail.com>
2015-03-28 15:24:58 +0000

    solution with map and sum

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import csv
 
+def last_of(lst):
+    return int(lst[-1])
+
 def get_at_bat_sum(filename):
-    sum = 0
     with open(filename) as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            sum += int(row[3])
-    return sum
+        list_of_lists = list(csv.reader(csvfile))
+    return sum(map(last_of, list_of_lists))
 
 def run(filename):
     summed = get_at_bat_sum(filename)
